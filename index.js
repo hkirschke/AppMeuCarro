f5813417b67acb9189faccafc4cc45076c6a953c
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-09-08 19:50:10 -0300

    Refactor

--- index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./src/config/custom-express');
-
-app.listen(3000, () => 
-    console.log("Servidor ouvindo a porta 3000")
-);
--- src/app/controllers/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    app.get('/', (req,res) =>
-        res.send("Requisição do tipo get na rota barra")
-    )
-}
