9f5635158a56ddb068597d8beab1f62f524d05f3
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-09-29 22:11:39 -0300

    base projeto
    craido models json
    icones
    ajuste webmanifest
    criado arquivos css

--- meucarro-sw.js
+++ meucarro-sw.js
@@ -7,3 +7,9 @@ self.addEventListener('activate'), function(event)
 {
     console.log('meucarro-sw ativo',event);
 }
+
+self.addEventListener('fetch'), function (e) 
+{
+    console.log('sw: carregando', e.request.url);
+    // e.respondWith(fecth('nomearquivo'));
+}
